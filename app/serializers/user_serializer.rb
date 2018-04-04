ad21cdffccc1332ebdfe5f3e9a1ede0c5b3b39d0
Dillion Verma <dillionverma@hotmail.com>
2018-04-04 11:46:43 -0700

    setup user serializer and annotate

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -17,17 +17,7 @@
 #  updated_at             :datetime         not null
 #
 
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-
-
-  def jwt_token
-    return unless Rails.env.development? || Rails.env.test?
-    JWT.encode({ user_token: id }, Rails.application.secrets.secret_key_base, 'HS256', {})
-  end
-
+class UserSerializer < ActiveModel::Serializer
+  attributes :id
+  attribute :email
 end
