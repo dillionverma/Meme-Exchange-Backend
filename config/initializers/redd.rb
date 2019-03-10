d810f9c9adb53d91a55078930f17b0aaded644d0
Dillion Verma <dillionverma@hotmail.com>
2019-03-09 16:32:50 -0800

    change up backend reddit client user agent

--- config/initializers/redd.rb
+++ config/initializers/redd.rb
@@ -1,2 +1,2 @@
-Reddit = Redd.it(user_agent: 'Memesx Client', client_id: ENV['reddit_client_id'], 
+Reddit = Redd.it(user_agent: 'Meme Exchange Client', client_id: ENV['reddit_client_id'],
                  secret: ENV['reddit_secret_key'])
