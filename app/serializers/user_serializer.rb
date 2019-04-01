04b848d60f804192f1ea0ac1c9b494bbf340a035
Dillion Verma <dillionverma@hotmail.com>
2019-03-31 23:27:38 -0700

    fix portfolio query pt

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -32,7 +32,7 @@ class UserSerializer < ActiveModel::Serializer
 
   def initialize(*args)
     super
-    @portfolio ||= self.object.meme_portfolios.joins(meme: :meme_histories).order(created_at: :desc).select("meme_portfolios.id, memes.reddit_id, memes.title, memes.subreddit, memes.author, meme_portfolios.quantity, meme_histories.price as price, meme_histories.price - meme_portfolios.cost as profit, memes.url")
+    @portfolio ||= self.object.meme_portfolios.joins("LEFT JOIN memes ON memes.id = meme_portfolios.meme_id LEFT JOIN LATERAL ( SELECT id, meme_id, created_at, price FROM meme_histories ORDER BY created_at DESC LIMIT 1) AS mh ON mh.meme_id = memes.id").order(created_at: :desc).select("meme_portfolios.id, memes.reddit_id, memes.title, memes.subreddit, memes.author, meme_portfolios.quantity, mh.price as price, price*meme_portfolios.quantity - meme_portfolios.cost as profit, memes.url")
   end
 
   def portfolio
