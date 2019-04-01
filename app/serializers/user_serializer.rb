9ac684342bf74ad8da1929280915dd6a347121ad
Dillion Verma <dillionverma@hotmail.com>
2019-04-01 01:10:29 -0700

    update user profile query completely

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -32,7 +32,33 @@ class UserSerializer < ActiveModel::Serializer
 
   def initialize(*args)
     super
-    @portfolio ||= self.object.meme_portfolios.joins("LEFT JOIN memes ON memes.id = meme_portfolios.meme_id LEFT JOIN LATERAL ( SELECT id, meme_id, created_at, price FROM meme_histories ORDER BY created_at DESC LIMIT 1) AS mh ON mh.meme_id = memes.id").order(created_at: :desc).select("meme_portfolios.id, memes.reddit_id, memes.title, memes.subreddit, memes.author, meme_portfolios.quantity, mh.price as price, price*meme_portfolios.quantity - meme_portfolios.cost as profit, memes.url")
+    # https://stackoverflow.com/questions/9796078/selecting-rows-ordered-by-some-column-and-distinct-on-another
+    # https://dev.mysql.com/doc/refman/5.6/en/example-maximum-column-group-row.html
+    @portfolio ||= self.object.meme_portfolios.joins("
+        LEFT JOIN memes ON memes.id = meme_portfolios.meme_id 
+        LEFT JOIN ( 
+          SELECT mh1.id, mh1.meme_id, mh1.created_at, mh1.price 
+          FROM meme_histories mh1
+              JOIN (
+                  SELECT meme_id, max(created_at) max_created_at
+                  FROM meme_histories
+                  GROUP BY meme_id
+              ) mh2
+          ON mh1.meme_id = mh2.meme_id AND mh1.created_at = mh2.max_created_at
+          ORDER BY mh1.created_at DESC
+        ) AS mh ON mh.meme_id = memes.id")
+      .select("
+        meme_portfolios.id, 
+        memes.reddit_id, 
+        memes.title, 
+        memes.subreddit, 
+        memes.author, 
+        meme_portfolios.quantity, 
+        mh.price as price, 
+        price*meme_portfolios.quantity - meme_portfolios.cost as profit, 
+        memes.url")
+      .order(created_at: :desc)
+
   end
 
   def portfolio
