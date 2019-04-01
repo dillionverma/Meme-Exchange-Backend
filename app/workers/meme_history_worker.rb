9b6091a04e6a36c37408b21316fdea441d337dcc
Dillion Verma <dillionverma@hotmail.com>
2019-04-01 00:03:33 -0700

    fix worker wrong created_at issue

--- app/workers/meme_history_worker.rb
+++ app/workers/meme_history_worker.rb
@@ -8,7 +8,7 @@ class MemeHistoryWorker
       memes.each_slice(1000) do |data|
         values = data.map do |meme|
           price = Reddit.get.from_ids('t3_' + meme[:reddit_id]).to_ary[0].score
-          "(#{meme[:id]},'#{meme[:reddit_id]}',#{price},'#{Time.now.to_s}','#{Time.now.to_s}','#{Time.now.to_s}')"
+          "(#{meme[:id]},'#{meme[:reddit_id]}',#{price},'#{Time.current.to_s}','#{Time.current.to_s}','#{Time.current.to_s}')"
         end.join(",")
 
         sql = "INSERT INTO meme_histories (meme_id, reddit_id, price, date, created_at, updated_at) VALUES #{values}"
